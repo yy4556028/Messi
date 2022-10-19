3827d7d56b31bf9490c9640e62f7564fbd1cf2aa
yang.yu <yang.yu@mobvoi.com>
2022-10-19 16:12:20 +0800

    饮食模块更新数据

--- app/src/main/java/com/yuyang/messi/kotlinui/diet/DietHomeActivity.kt
+++ app/src/main/java/com/yuyang/messi/kotlinui/diet/DietHomeActivity.kt
@@ -30,19 +30,18 @@ class DietHomeActivity : AppBaseActivity() {
         binding.btnIngredients.setOnClickListener {
             startActivity(Intent(this, IngredientsActivity::class.java))
         }
-        binding.btnRecommendedVegetables.setOnClickListener {
+        binding.btnRecommendedFoods.setOnClickListener {
             MaterialAlertDialogBuilder(this)
-                .setTitle("推荐蔬菜")
+                .setTitle("推荐食物")
                 .setMessage(
-                    "铁：茼蒿\n" +
-                            "钙：油菜\n" +
-                            "叶酸：菠菜\n" +
-                            "蛋白质：莲藕\n" +
-                            "纤维素：红薯\n" +
-                            "高纤维素：豌豆\n" +
-                            "维生素C：番茄\n" +
-                            "维生素B：芹菜\n" +
-                            "胡萝卜素：西兰花\n"
+                    "健康食品：番茄，菠菜，大蒜，花生，菜花，蓝莓，燕麦，绿茶，红酒，桂鱼\n\n" +
+                            "水果：木瓜，草莓，橙子，柑橘，猕猴桃，芒果，杏，柿子，西瓜\n\n" +
+                            "蔬菜：红薯，芦笋，白菜，花椰菜，芹菜，茄子，甜菜，胡萝卜，荠菜，大花蕙兰，金针菇，雪红，大白菜\n\n" +
+                            "肉类：鸡鸭鹅鱼兔\n\n" +
+                            "健脑：菠菜，韭菜，南瓜，洋葱，西兰花，辣椒，豌豆，番茄，胡萝卜，小青菜，蒜苗，芹菜，贝壳类(核桃，花生，开心果，腰果，松子，杏仁，大豆)，糙米，猪肝等\n\n" +
+                            "心脏：每天吃50g鱼可减少40%心脏病发病率，尤其深海鱼。野生马齿苋\n\n" +
+
+                            "BBC评健康：杏仁，释迦，海鲈鱼，多宝鱼，奇亚籽，南瓜籽，猪乸菜，肥猪肉，甜菜叶，鲷鱼"
                 )
                 .setCancelable(true)
                 .setPositiveButton("确定") { _, _ -> }
@@ -98,15 +97,49 @@ class DietHomeActivity : AppBaseActivity() {
                 .setTitle("维生素")
                 .setMessage(
                     "维生素A：脂溶性。大多数人不缺除非非洲难民。高剂量补充可能增加健康人群或疾病患者死亡率，食物获取就可以(鸡蛋黄，牛奶，动物肝脏，胡萝卜，玉米，菠菜)\n\n" +
-                            "维生素B：水溶性。爱喝酒 吃东西不全面 吃素 可以补\n\n" +
+                            "维生素B：水溶性。爱喝酒、吃东西不全面、吃素的可以补\n\n" +
                             "维生素B1：玉米燕麦豆类\n\n" +
                             "维生素B2：牛奶鸡蛋动物肝脏\n\n" +
                             "维生素B3：新鲜的蔬菜水果\n\n" +
+                            "维生素B4：动物内脏、肉类、豆制品、虾、沙丁鱼、蚝、菠菜、黑木耳、鱿鱼、蘑菇等\n\n" +
+                            "维生素B5：食物中几乎无所不在\n\n" +
+                            "维生素B6：白肉含量较高\n\n" +
+                            "维生素B7：动物肝脏、动物肾脏、大豆粉、牛奶、羊奶、蛋黄。水果含量较少\n\n" +
+                            "维生素B8：啤酒酵母、动物肝脏、全谷物、哈密瓜、柑橘类水果（柠檬除外）、香蕉、小麦胚芽、坚果、豆类、葡萄干、卷心菜等。人体可自己产生\n\n" +
                             "维生素B9(叶酸)：新鲜的蔬菜水果。预防胎儿神经管缺陷。孕妇0.4mg/日\n\n" +
                             "维生素B12：肉\n\n" +
                             "维生素C：水溶性。过量补充会增加结石风险。如果蔬菜水果吃得少可以适当补\n\n" +
                             "维生素D：脂溶性。含维生D食物少。很多人基本都缺。帮助钙吸收。\n\n" +
                             "维生素E：脂溶性。缺乏很罕见。高剂量会在体内蓄积中毒增加死亡率"
+                )
+                .setCancelable(true)
+                .setPositiveButton("确定") { _, _ -> }
+                .setNegativeButton("取消") { _, _ -> }
+                .show()
+        }
+        binding.btnGB.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("执行标准")
+                .setMessage(
+                    "大米：\n" +
+                            "GB/T 1354 普通大米 不好\n" +
+                            "GB/T 19266 五常大米\n" +
+                            "GB/T 18824 盘锦大米\n" +
+                            "GB/T 22438 原阳大米\n\n" +
+
+                            "油：\n" +
+                            "GB/T 2716 调和油 不好\n" +
+                            "GB/T 23347 橄榄油\n" +
+                            "GB/T 1536 菜籽油\n" +
+                            "GB/T 1534 花生油\n" +
+                            "GB/T 1535 大豆油\n" +
+                            "GB/T 19111 玉米油\n" +
+                            "GB/T 10464 葵花籽油\n\n" +
+
+                            "面粉：\n" +
+                            "GB/T 8607 高筋面粉\n" +
+                            "GB/T 1355 中筋面粉\n" +
+                            "GB/T 8608 低筋面粉"
                 )
                 .setCancelable(true)
                 .setPositiveButton("确定") { _, _ -> }
